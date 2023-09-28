8795f8ffe98de27cc4cdfee0c9aeb37b199a1a48
kwonMInwoo <qwerty029369@naver.com>
2023-09-28 12:13:18 +0900

    feat: 개발자 생성 기능 추가
    개발자 생성 기능 Controller를 추가했고, Dto를 만들어 Validation까지 진행

--- dmaker/src/main/java/study/spring/dmaker/controller/DMakerController.java
+++ dmaker/src/main/java/study/spring/dmaker/controller/DMakerController.java
@@ -1,12 +1,14 @@
 package study.spring.dmaker.controller;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import study.spring.dmaker.dto.CreateDeveloper;
 import study.spring.dmaker.service.DMakerService;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -15,9 +17,10 @@ import java.util.List;
 public class DMakerController {
     private final DMakerService dMakerService;
 
-    @GetMapping("/create-developer")
-    public List<String> createDeveloper(){
-        dMakerService.createDeveloper();
+    @PostMapping("/create-developer")
+    public List<String> createDeveloper(@Valid @RequestBody CreateDeveloper.Request request){
+        log.info("request : {}", request);
+        dMakerService.createDeveloper(request);
         return List.of("Olaf");
     }
 }
