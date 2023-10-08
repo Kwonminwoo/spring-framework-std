546bce104020fd976ab2e687cc110858899b6d8d
KwonMinwoo <qwerty029369@naver.com>
2023-10-08 16:06:32 +0900

    feat: init java + gradle
    자바, gradle을 최초 설정함

---
+++ springboot-std/src/main/java/com/std/springbootstd/SpringbootStdApplication.java
@@ -0,0 +1,13 @@
+package com.std.springbootstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootStdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootStdApplication.class, args);
+	}
+
+}
