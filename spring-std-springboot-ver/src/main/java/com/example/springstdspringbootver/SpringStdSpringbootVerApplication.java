e0b046db1c873c49a7b7b8e8826ff37a29765d03
KwonMinwoo <qwerty029369@naver.com>
2023-10-10 23:26:16 +0900

    feat: 스프링 부트 버전 최초 생성 및 기존의 기본 코드 생성
    스프링 부트로 프로젝트를 시작하여 스프링에서 작성한 기본 코드를 가져옴

---
+++ spring-std-springboot-ver/src/main/java/com/example/springstdspringbootver/SpringStdSpringbootVerApplication.java
@@ -0,0 +1,13 @@
+package com.example.springstdspringbootver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStdSpringbootVerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStdSpringbootVerApplication.class, args);
+    }
+
+}
---
+++ spring-std-springboot-ver/src/test/java/com/example/springstdspringbootver/SpringStdSpringbootVerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springstdspringbootver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStdSpringbootVerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
