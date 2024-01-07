afc7a0c1849471b21a67f28c69fe26aa472d3a63
kwonMInwoo <qwerty029369@naver.com>
2024-01-07 21:42:00 +0900

    feat: 기본 엔티티 생성
    프로젝트에 필요한 엔티티 세팅

---
+++ queryDsl-std/src/main/java/com/example/querydslstd/entity/Hello.java
@@ -0,0 +1,16 @@
+package com.example.querydslstd.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Getter;
+
+@Entity
+@Getter
+public class Hello {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+}
