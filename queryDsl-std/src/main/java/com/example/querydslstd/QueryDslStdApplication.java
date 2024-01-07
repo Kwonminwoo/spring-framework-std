7de862a94bc0543ad275bda0ec52cfe9b4bdda00
kwonMInwoo <qwerty029369@naver.com>
2024-01-07 21:40:36 +0900

    feat: 쿼리dsl 프로젝트  init
    프로젝트 생성 및 설정 파일 작성

---
+++ queryDsl-std/src/main/java/com/example/querydslstd/QueryDslStdApplication.java
@@ -0,0 +1,13 @@
+package com.example.querydslstd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueryDslStdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueryDslStdApplication.class, args);
+	}
+
+}
---
+++ queryDsl-std/src/test/java/com/example/querydslstd/QueryDslStdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.querydslstd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QueryDslStdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
